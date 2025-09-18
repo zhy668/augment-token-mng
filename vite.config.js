0b4e9f521b666c13b51b7fbef3a1bdfbd5e06560
zhaocheng <zhaochengcube@gmail.com>
2025-09-18 11:52:09 +0800

    添加@intlify/unplugin-vue-i18n插件以支持国际化，更新Vite配置以包含语言文件，优化package.json和package-lock.json以反映新依赖项。

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,19 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import { resolve } from "path";
+import VueI18nPlugin from "@intlify/unplugin-vue-i18n/vite";
 
 // https://vitejs.dev/config/
 export default defineConfig(async () => ({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    VueI18nPlugin({
+      include: [
+        resolve(__dirname, "./src/locales/zh-CN.js"),
+        resolve(__dirname, "./src/locales/en-US.js"),
+      ],
+    }),
+  ],
 
   // Vite options tailored for Tauri development and only applied in `tauri dev` or `tauri build`
   //
